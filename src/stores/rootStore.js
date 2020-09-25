bb6139bb3014df5037f6166f83edba8e4ca6e7ef
Jordi Martí Domínguez <jmarti@MacBook-Pro-de-Jordi.local>
2020-09-24 20:29:20 -0500

    redux-thunk implementation

--- src/stores/rootStore.js
+++ src/stores/rootStore.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 
 import rootReducer from "reducers/rootReducer";
 
@@ -6,6 +8,9 @@ import rootReducer from "reducers/rootReducer";
 export default createStore(
     rootReducer,
     applyMiddleware(
-        
+        ...[
+            thunk,
+            process.env.NODE_ENV === 'development' ? createLogger() : null
+        ].filter(m => m)
     )
 );
